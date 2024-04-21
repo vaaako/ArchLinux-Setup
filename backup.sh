d185a32c4b0fefada0657f558c098216b220cc1d
vaaako <matharth520@gmail.com>
2024-04-20 23:56:50 -0300

    configs

--- backup.sh
+++ backup.sh
@@ -1,10 +1,19 @@
 CONFIG_DIR="config_files"
 
-NEOFETCH="${HOME}/.config/neofetch"
-KITTY="${HOME}/.config/kitty"
-ZSH="${HOME}/.config/zsh"
+NEOFETCH=~/.config/neofetch
+KITTY=~/.config/kitty
+ZSH=~/.config/zsh
 
+# Remove old files
+rm -rf $CONFIG_DIR/kitty $CONFIG_DIR/neofetch
+
+# Make dirs
+mkdir $CONFIG_DIR/kitty $CONFIG_DIR/neofetch
+
+# Add new files
 cp $NEOFETCH/*.txt $NEOFETCH/config.conf $CONFIG_DIR/neofetch
 cp $KITTY/*.conf $CONFIG_DIR/kitty
-cp $HOME/.zshrc $CONFIG_DIR/zshrc.txt
-cp $HOME/.bashrc $CONFIG_DIR/bashrc.txt
+cp ~/.zshrc $CONFIG_DIR/zshrc.txt
+cp ~/.bashrc $CONFIG_DIR/bashrc.txt
+
+# TODO -- Zip xfce4 config files
