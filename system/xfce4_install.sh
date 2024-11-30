b9d1d1a55cad55ace097badc5a2027e4c295a99a
vako <vakofernanda@gmail.com>
2024-11-30 16:19:51 -0300

    improved system scripts

--- system/xfce4_install.sh
+++ system/xfce4_install.sh
@@ -6,7 +6,7 @@ echo -e "\033[1m~> Installing the following packages:\033[0m"
 echo "- xorg: X Window System"
 echo "- xfce4: XFCE desktop environment"
 echo "- kitty: Terminal emulator"
-echo "- xfce4-terminal: XFCE terminal emulator"
+# echo "- xfce4-terminal: XFCE terminal emulator"
 echo "- xfce4-goodies: Additional XFCE goodies"
 echo "- xfce4-whiskermenu-plugin: Whisker menu plugin for XFCE"
 echo "- lightdm-gtk-greeter: GTK greeter for LightDM"
@@ -14,5 +14,5 @@ echo "- lightdm-gtk-greeter-settings: Settings manager for LightDM GTK greeter"
 echo "================================="
 echo
 
-sudo pacman -S --needed xorg xfce4 kitty xfce4-terminal xfce4-goodies xfce4-whiskermenu-plugin lightdm-gtk-greeter lightdm-gtk-greeter-settings
+sudo pacman -S --needed xorg xfce4 kitty xfce4-goodies xfce4-whiskermenu-plugin lightdm-gtk-greeter lightdm-gtk-greeter-settings
 
